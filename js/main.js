d7442748deac7a09c2c67a98f6b15a0fdde6677f
Victoria Latimer <victorialatimer@Victorias-MacBook-Pro.local>
2018-04-19 11:54:37 -0500

    initial commit.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// https://ghibliapi.herokuapp.com/films
+
+//var url = "http://api.openweathermap.org/data/2.5/weather?zip=37216&appid=ffbaed99f10375462add667213061774";
+
+//$("#getInfoBtn").click(function(){
+	//if( $("#zipInput").val()length == 5 ){
+	//	$.getJSON(url + $("#zipInput").val(),function(result){
+	//		$('#cityName').html(result.name);
+	//		$('#conditions').html(result.weather[0].description);
+	//		if(result.main.temp <65){
+	//			$('#temp').html(result.main.temp + "˚");
+	//			$('#temp').css('color', 'red');
+	//		}
+	//		var d = new Date(result.dt * 1000);
+	//		$('#time').html( d.toDateString() + "<br />" + d.toLocaleTimeString() );
+	//		$('#weatherIcon').attr("src","http://openweathermap.org/img/w/" + result.weather[0].icon + ".png")
+	//		$('#weatherInfo').css('animation-name', "fadeIn");
+	//});
+
+//} else {
+	//alert("PLEASE ENTER THE CORRECT NUMBER OF CHARACTERS!");
+//}
+
+//});
